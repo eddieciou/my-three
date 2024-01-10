fbd981869c3fa19650bfa92fa20477547dc9bb30
eddie <a589658gur@gmail.com>
2024-01-10 15:18:04 +0800

    使用Three.js做一個持續選轉的立方塊

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,50 @@
+import * as Three from 'three';
+import { useEffect } from 'react';
+
 const App = () => {
-  return <div className="bg-rose-500">React + Typescript + Vite</div>;
+	useEffect(() => {
+		// 創建場景
+		const scene = new Three.Scene();
+
+		// 創建相機
+		const camera = new Three.PerspectiveCamera(
+			45, // 視角
+			window.innerWidth / window.innerHeight, // 寬高比
+			0.1, // 近平面
+			1000, //	遠平面
+		);
+
+		// 創建渲染器
+		const renderer = new Three.WebGL1Renderer();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		document.body.appendChild(renderer.domElement);
+
+		// 創建幾何體
+		const geometry = new Three.BoxGeometry(1, 1, 1);
+		// 創建材質
+		const material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+		// 創建網格
+		const cube = new Three.Mesh(geometry, material);
+		scene.add(cube);
+
+		// 設定相機位置
+		camera.position.z = 5;
+		camera.lookAt(0, 0, 0);
+
+		// 渲染函數
+		const animate = () => {
+			requestAnimationFrame(animate);
+			cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
+
+			// 渲染
+			renderer.render(scene, camera);
+		};
+
+		animate();
+	}, []);
+
+	return <div></div>;
 };
 
 export default App;
